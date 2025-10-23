1d724a22d86a4f994bb70cb72ff8d4d453e7dea9
mrcool7387 <146818600+mrcool7387@users.noreply.github.com>
2025-10-23 19:01:05 +0200

    Added TEST {.cpp|.h} files and Added Global Functions

---
+++ Source/Guns_And_Choices/EncryptDecrypt.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "EncryptDecrypt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNS_AND_CHOICES_API UEncryptDecrypt : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Guns_And_Choices/EncryptDecryptFile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EncryptDecrypt.h"
+#include "EncryptDecryptFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNS_AND_CHOICES_API UEncryptDecryptFile : public UEncryptDecrypt
+{
+	GENERATED_BODY()
+	
+};
