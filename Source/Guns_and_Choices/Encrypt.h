50b902e2863e789938963291c9aedb67a691b1e3
LordSimon414 <lordsimon414@gmail.com>
2026-02-08 15:33:15 +0100

    Push

---
+++ Source/Guns_and_Choices/Encrypt.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Encrypt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNS_AND_CHOICES_API UEncrypt : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
